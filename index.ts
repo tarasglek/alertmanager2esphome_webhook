944f035c2c1561aef012be163a0b68da66856281
Taras Glek <taras@glek.net>
2020-11-29 14:08:24 -0800

    url params, etc

--- index.ts
+++ index.ts
@@ -4,30 +4,40 @@ const app = express()
 const port = 9000
 app.use(express.json());
 
+let url_param_re = /^url_param_(.*)/
 app.post('/alert', (req, res) => {
     let totalBody = ""
     for (let alert of req.body.alerts) {
         console.log(alert)
         let destURL:string = ""
         let status = alert.status // firing or resolved
-        try {
-            destURL = alert.annotations[status]
-        } catch(e) {
-            continue;
+        if (!(status in alert.annotations))
+            continue
+        destURL = alert.annotations[status]
+        for (const [key, value] of Object.entries(alert.labels)) {
+            let match = url_param_re.exec(key)
+            if (!match)
+                continue
+            let param = match[1]
+            let val = encodeURIComponent(value as string)
+            if (destURL.indexOf('?') == -1) {
+                destURL = destURL + "?"
+            }
+            let last_char = destURL[destURL.length - 1]
+            if (!(last_char == '?' || last_char == '&'))
+                destURL = destURL + "&"
+            destURL += param + '=' + val
         }
-        
         const options = {
             url: destURL,
             method: alert.annotations.method
         };
-        console.log(options)
 
         request(options, (err, res, body) => {
             if (err) {
                 return console.log(err);
             }
-            console.log(`Status: ${res.statusCode}`);
-            console.log(body);
+            console.log(`${options.method} ${destURL} Status: ${res.statusCode}`);
             totalBody += body
         });
     }
